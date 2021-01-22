e07bb090c8a7d729e90b0d52a70047744ace086f
kondala naidu <mnnaidu63@gmail.com>
2021-01-22 11:45:28 +0530

    First commit

---
+++ assignment_operations.py
@@ -0,0 +1,21 @@
+# Assignment operators on Number
+n = 9
+print(n)
+n += 2
+print(n)
+n=n+2
+print(n)
+n-=2
+print(n)
+n*=2
+print(n)
+n/=2
+print(n)
+n//=2
+print(n)
+n%=2
+print(n)
+n+=99
+print(n)
+n%=2
+print(n)
